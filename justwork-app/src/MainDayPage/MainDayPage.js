aec710cf79eb8425c0ff20e80324d3bc415c9538
Ervin Enriquez <ervinjenriquez@yahoo.com>
2022-08-25 00:56:48 -0400

    Feat: Days are displayed on the MainDayPage with a Table, showing ID, Title, & Description

--- justwork-app/src/MainDayPage/MainDayPage.js
+++ justwork-app/src/MainDayPage/MainDayPage.js
@@ -1,26 +1,32 @@
 import React, { useEffect, useState } from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-export default function MainDayPage() {
+export default function MainDayPage(props) {
 
     const [DayList, setDayList] = useState([]);
 
     //localhost:8080/days
 
-    useEffect(()=>{
-        const url ='//localhost:8080/days'
+    useEffect(() => {
+        const url = '//localhost:8080/days'
 
         const fetchData = async () => {
             try {
-              const response = await fetch(url);
-              const json = await response.json();
-              console.log(json);
-              setDayList(json);
+                const response = await fetch(url);
+                const json = await response.json();
+                setDayList(json);
             } catch (error) {
-              console.log("error", error);
+                console.log("error", error);
             }
-          };
-      
-          fetchData();
+        };
+
+        fetchData();
     }, []);
 
 
@@ -30,11 +36,36 @@ export default function MainDayPage() {
             <ul>
                 {
                     DayList.map((day, key) => {
-                        return <li key={key}>{day.title}</li>
+                        return <li key={key}>{day.title} and {day.description} and its ID {day.id}</li>
                     })
                 }
             </ul>
 
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="middle">ID</TableCell>
+                            <TableCell align="middle">Title</TableCell>
+                            <TableCell align="middle">Description</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {DayList.map((day) => (
+                            <TableRow
+                                key={day.title}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {day.id}
+                                </TableCell>
+                                <TableCell align="left">{day.title}</TableCell>
+                                <TableCell align="left">{day.description}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </>
     )
 }
