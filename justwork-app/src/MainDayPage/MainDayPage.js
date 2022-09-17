a02906e17efee198b878b031e627f088e2108240
Justin Lepiten <justinlepiten@Justins-MacBook-Air.local>
2022-09-17 01:39:17 -0400

    Feat: Button to add a new day. TODO: error handling.

--- justwork-app/src/MainDayPage/MainDayPage.js
+++ justwork-app/src/MainDayPage/MainDayPage.js
@@ -6,16 +6,25 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import TextField from '@mui/material/TextField';
+
 
 export default function MainDayPage(props) {
 
     const [DayList, setDayList] = useState([]);
+    const [openNewDay, setOpenNewDay] = useState(false);
+    const [title, setTitle] = useState(null);
+    const [description, setDescription] = useState(null);
 
-    //localhost:8080/days
+    const url = '//localhost:8080/days';
 
     useEffect(() => {
-        const url = '//localhost:8080/days'
-
         const fetchData = async () => {
             try {
                 const response = await fetch(url);
@@ -25,9 +34,36 @@ export default function MainDayPage(props) {
                 console.log("error", error);
             }
         };
-
         fetchData();
     }, []);
+
+    const handleClickNewDay = () => {
+        setOpenNewDay(true);
+    };
+
+    const handleCloseNewDay = () => {
+        setOpenNewDay(false);
+    };
+
+    const handleConfirmNewDay = async () => {
+        const day = { title, description };
+        const request = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify(day)
+        };
+
+        const response = await fetch(url, request);
+        const newDay = await response.json();
+        setDayList(oldDays => [...oldDays, newDay]);
+        console.log('new day added');
+
+        // Resets text fields.
+        setTitle("");
+        setDescription("");
+
+        handleCloseNewDay();
+    };
 
 
     return (
@@ -58,6 +94,41 @@ export default function MainDayPage(props) {
                     </TableBody>
                 </Table>
             </TableContainer>
+            <Button variant="outlined" onClick={handleClickNewDay}>
+                New Day
+            </Button>
+            <Dialog open={openNewDay} onClose={handleCloseNewDay}>
+                <DialogTitle>Add a New Day</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        To add a new day to your workout, please enter a title and description here.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        required
+                        fullWidth
+                        margin="dense"
+                        id="title"
+                        label="Title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <TextField
+                        required
+                        fullWidth
+                        multiline
+                        margin="dense"
+                        id="description"
+                        label="Description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseNewDay}>Cancel</Button>
+                    <Button onClick={handleConfirmNewDay}>Confirm</Button>
+                </DialogActions>
+            </Dialog>
         </>
     )
 }
