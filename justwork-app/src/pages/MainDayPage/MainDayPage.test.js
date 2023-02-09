d6d91c2bc0f1e2a106673bc9b60ce4e0b4895ea9
Jim <jgmedr0@gmail.com>
2023-02-09 13:50:26 -0600

    Test:Wrap component in MemoryRouter

--- justwork-app/src/pages/MainDayPage/MainDayPage.test.js
+++ justwork-app/src/pages/MainDayPage/MainDayPage.test.js
@@ -1,42 +1,49 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { Route, Routes, Router } from "react-router-dom";
-import createMemoryHistory from "history";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
-import MainDayPage from './MainDayPage';
+import MainDayPage from "./MainDayPage";
 
-describe('MainDayPage renders and has the corect coloumns', () => {
-    let originalFetch;
+describe("MainDayPage renders and has the corect coloumns", () => {
+  let originalFetch;
 
-    beforeEach(() => {
-        originalFetch = global.fetch;
-        global.fetch = jest.fn(() => Promise.resolve({
-            json: () => Promise.resolve([
-                {
-                    id: 1,
-                    exercises: [],
-                    title: "Mock Title1",
-                    description: "Mock Description1"
-                },
-                {
-                    id: 2,
-                    exercises: [],
-                    title: "Mock Title2",
-                    description: "Mock Description2"
-                }
-            ])
-        }));
-    });
+  beforeEach(() => {
+    originalFetch = global.fetch;
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve([
+            {
+              id: 1,
+              exercises: [],
+              title: "Mock Title1",
+              description: "Mock Description1",
+            },
+            {
+              id: 2,
+              exercises: [],
+              title: "Mock Title2",
+              description: "Mock Description2",
+            },
+          ]),
+      })
+    );
+  });
 
-    afterEach(() => {
-        global.fetch = originalFetch;
-    });
+  afterEach(() => {
+    global.fetch = originalFetch;
+  });
 
-    it('Should have the mocked values in the correct locations', async () => {
-
-        render(<Router><MainDayPage /></Router>);
-        const description = await screen.findByTestId('IndividualDayDescription');
-        expect(description.textContent).toBe("Mock Description1");
-
-    })
-})
+  it("Should have the mocked values in the correct locations", async () => {
+    const history = createMemoryHistory(); // we need to define a mock history to the router
+    render(
+      <MemoryRouter history={history}> 
+        <MainDayPage />
+      </MemoryRouter>
+    );// MemoryRouter is more performant for tests
+    const description = await screen.findAllByTestId("IndividualDayDescription");
+    expect(description[0].textContent).toBe("Mock Description1");
+    expect(description[1].textContent).toBe("Mock Description2");
+  });
+});
